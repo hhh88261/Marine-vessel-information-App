bd799c488e8708d7ed610a85b7f7c2e6a3ae7748
thkim8209 <hhh88261@gmail.com>
2025-03-25 15:13:04 +0900

    add AisMap.vue route

--- src/components/member/LoginForm.vue
+++ src/components/member/LoginForm.vue
@@ -2,28 +2,64 @@
   <div class="flex items-center justify-center h-screen bg-gray-100">
     <div class="bg-white p-6 rounded-lg shadow-md w-96">
       <h2 class="text-2xl font-bold mb-4 text-center">로그인</h2>
-      <form @submit.prevent="handleLogin">
         <div class="mb-4">
           <label class="block text-gray-700">아이디</label>
-          <input type="loginId" v-model="email" class="w-full p-2 border rounded mt-1"/>
+          <input type="text" v-model="username" class="w-full p-2 border rounded mt-1"/>
         </div>
         <div class="mb-4">
           <label class="block text-gray-700">비밀번호</label>
           <input type="password" v-model="password" class="w-full p-2 border rounded mt-1"/>
         </div>
-        <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded">로그인</button>
+        <button type="submit" @click="handleLogin" class="w-full bg-blue-500 text-white p-2 rounded" >로그인</button>
         <button @click="joinRouter">회원가입</button>
-      </form>
     </div>
   </div>
 </template>
 
 <script setup>
-import {useRouter} from "vue-router";
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
 
 const router = useRouter();
 
+const username = ref('');
+const password = ref('');
+
 const joinRouter = () => {
   router.push('/signup');
-}
+};
+
+// 로그인 API 호출 함수
+const handleLogin = async () => {
+  console.log(username.value,password.value)
+  try {
+    const requestData = {
+      username: username.value,
+      password: password.value,
+    };
+    const response = await axios.post('http://localhost:8888/login', requestData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    // 응답 헤더에서 Authorization 토큰 가져오기
+    const token = response.headers['authorization'];
+
+    if (token) {
+      // 토큰을 로컬 스토리지 또는 Vuex/Pinia에 저장
+      localStorage.setItem('token', token);
+
+      console.log('로그인 성공, 토큰:', token);
+
+      // AisMap.vue로 이동
+      router.push('/AisMap');
+    } else {
+      console.error('토큰이 응답에 없음');
+      alert('로그인 실패!');
+    }
+  } catch (error) {
+    console.log('API ERROR!', error);
+  }
+};
 </script>
--- src/router/MemberRouter.js
+++ src/router/MemberRouter.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginForm from "@/components/member/LoginForm.vue";
 import SignUp from "@/components/member/SignUp.vue";
+import AisMap from "@/components/AisMap.vue";
 
 const routes = [
     { path: '/', component: LoginForm },
-    { path: '/signup', component: SignUp }
+    { path: '/signup', component: SignUp },
+    { path: '/main', component: AisMap}
 ];
 
 const router = createRouter({
